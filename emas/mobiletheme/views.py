f99346e82d8b1b24914e634ed0acfe93971d005e
Roché Compaan <roche@upfrontsystems.co.za>
2012-05-16 15:04:38 +0200

    specialise MobileTool to always treat MXit WebBot as low end phone

--- emas/mobiletheme/views.py
+++ emas/mobiletheme/views.py
@@ -1,6 +1,8 @@
+from zope.interface import Interface
 from five import grok
 from plone.directives import form
 from rhaptos.xmlfile.xmlfile import IXMLFile
+from gomobile.mobile.browser.views import MobileTool as BaseMobileTool
 from interfaces import IThemeLayer
 
 grok.templatedir('templates')
@@ -11,3 +13,18 @@ class XMLFile(form.DisplayForm):
     grok.require('zope2.View')
     grok.name('mobilexmlfile')
     grok.template('xmlfile')
+
+
+class MobileTool(BaseMobileTool):
+    """ Specialise check for low end phones
+    """
+
+    def isLowEndPhone(self):
+        """ override to check for Mxit """
+        if self.isMXit():
+            return True
+        else:
+            return super(MobileTool, self).isLowEndPhone()
+
+    def isMXit(self):
+        return "MXit WebBot" in self.request.get('HTTP_USER_AGENT', '')
