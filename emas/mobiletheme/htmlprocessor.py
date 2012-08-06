541ae928bc2e73645c04533f73a54a488bc136d8
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-08-06 15:11:51 +0200

    Remove debug switch

--- emas/mobiletheme/htmlprocessor.py
+++ emas/mobiletheme/htmlprocessor.py
@@ -473,7 +473,7 @@ class HTMLImageRewriter(BrowserView):
 
         mob_tool = getMultiAdapter((self.context, self.request),
                                    name='mobile_tool')
-        if not mob_tool.isMXit():
+        if mob_tool.isMXit():
             mxitprocessor = MxitHTMLProcessor(self.context, self.request)
             html = mxitprocessor.process(html)
 
