673ad9a5d22d1036db752eccb1f2fd345d5ea719
Roché Compaan <roche@upfrontsystems.co.za>
2012-08-26 14:49:00 +0200

    remove hardcoded port number

--- emas/mobiletheme/tracking/views.py
+++ emas/mobiletheme/tracking/views.py
@@ -69,5 +69,5 @@ class Tracking_Image(grok.View):
             unique_id = int(hashlib.md5(remote_address).hexdigest(), 16)
         entry['unique_id'] = unique_id
 
-        gaq = get_q(q_name='google_analytics_q', port=6666)
+        gaq = get_q(q_name='google_analytics_q')
         gaq.enqueue(GoogleQueue.deliver, entry)
