ca1c8c563dd0f35e922ff8cccc582d6068f8d61a
Roché Compaan <roche@upfrontsystems.co.za>
2012-09-21 13:44:31 +0200

    implement simple dashboard for mobile practice

--- emas/mobiletheme/practice.py
+++ emas/mobiletheme/practice.py
@@ -1,3 +1,5 @@
+import lxml
+
 from zope.interface import Interface
 from Products.CMFCore.utils import getToolByName
 
@@ -13,7 +15,7 @@ from interfaces import IThemeLayer
 PRACTICE_URL = '@@practice'
 
 
-class MXitPractice(BasePractice):
+class MobilePractice(BasePractice):
     """
         Custom view for mobile practice service
     """
@@ -26,3 +28,24 @@ class MXitPractice(BasePractice):
         """
         path = self.context.getPhysicalPath()
         return PRACTICE_URL in path.split('/')
+
+    def update(self):
+        path = self.request.get_header('PATH_INFO')
+        self.dashboard = path.endswith('dashboard')
+
+        if not self.dashboard:
+            return
+
+        html = lxml.html.fromstring(self.html)
+
+        self.booktitle = html.find('.//*[@id="dashboard-book-title"]').text
+        sections = []
+        for section in html.findall(
+                './/*[@class="dashboard-section-title-1"]/a'):
+            sections.append({
+                'href': section.get('href'),
+                'alttitle': "Click to practice %s" % section.text,
+                'title': section.text
+                }
+            )
+        self.sections = sections
